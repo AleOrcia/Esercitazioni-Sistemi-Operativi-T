e2eea3a5cdc59c2b1f40ba464f1ba4f5485b2f40
AleOrcia <92159043+AleOrcia@users.noreply.github.com>
2023-04-19 14:22:26 +0200

    Update ese6-2.sh

--- ese6-2.sh
+++ ese6-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]
-then echo SINTASSIII; exit
+    then echo SINTASSIII; exit
 fi
 
 username=$1
@@ -11,21 +11,21 @@ X=0
 for dir in $*
 do
 
-if [[ -d $dir ]]
-then echo OK
-else echo "Directory non esistente!"; exit
-fi
+    if [[ -d $dir ]]
+        then echo OK
+        else echo "Directory non esistente!"; exit
+    fi
 
-test=`ls -l $dir | awk '{print $3}'`
+    test=`ls -l $dir | awk '{print $3}'`
 
-for i in $test
-do
-    if [[ $i = $username ]]
-    then echo "$dir"
-    X=`expr $X + 1`
+    for i in $test
+    do
+        if [[ $i = $username ]]
+            then echo "$dir"
+            X=`expr $X + 1`
     
-fi 
-done
+        fi 
+    done
 
 done
 
