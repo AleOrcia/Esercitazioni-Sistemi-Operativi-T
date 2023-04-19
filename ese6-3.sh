4d2e4b136db18a0ce96502927a86a33dd5c27422
AleOrcia <92159043+AleOrcia@users.noreply.github.com>
2023-04-19 14:20:12 +0200

    Update ese6-3.sh

--- ese6-3.sh
+++ ese6-3.sh
@@ -9,27 +9,36 @@ M=$1
 stringa=$2
 filedirectory=$3
 
+if [[ $M =~ ^[0-9]+$ ]]
+    then echo OK
+    else echo "Non hai inserito un numero intero!"; exit
+fi
+
+if [ -r $filedirectory ]
+    then echo OK
+    else echo "Il file non esiste"; exit
+fi
+
 for i in `cat $filedirectory`
-do
-cd $i
+    do
+    cd $i
 
-for j in `ls`
-do
-if [[ -d $j ]]
-then continue;
-fi
+    for j in `ls`
+        do
+        if [[ -d $j ]]
+            then continue;
+        fi
 
-occorrenze=`grep -o $stringa $j | wc -l`
+        occorrenze=`grep -o $stringa $j | wc -l`
 
-if [ $occorrenze -gt $M ]
-then
-    dim=`ls -sh $j | awk '{print $1}'`
-    echo "Il file $j nella directory $i contiene $dim di caratteri"
-else
-    echo "Non abbastanza occorrenze"
-fi
-
-done
+        if [ $occorrenze -gt $M ]
+            then
+                dim=`ls -sh $j | awk '{print $1}'`
+                echo "Il file $j nella directory $i contiene $dim di caratteri"
+            else
+                echo "Non abbastanza occorrenze"
+        fi
+    done
 
 done
 
